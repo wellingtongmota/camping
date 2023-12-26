2f17dcf88eae9b13a606e8ff665e62977dc47c71
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-12-26 17:02:58 -0300

    Criação rota Admin

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import Home from './routes/Home.jsx'
-import Login from './routes/Login.jsx'
 import theme from './utils/theme.js'
+import Home from './routes/Home.jsx'
+import Admin from './routes/Admin.jsx'
+import Login from './routes/Login.jsx'
 
 const router = createBrowserRouter([
   {
@@ -13,7 +14,8 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: '/camping-fonte', element: <Home /> },
-      { path: '/camping-fonte/login', element: <Login /> }
+      { path: '/camping-fonte/admin', element: <Admin /> },
+      { path: '/camping-fonte/login', element: <Login /> },
     ]
   }
 ])
