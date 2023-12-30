d846b85deb2fb8f8929253651d3e627e458794e3
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-12-30 14:30:37 -0300

    Configuração emailjs

--- src/components/questions/QuestionsForm.jsx
+++ src/components/questions/QuestionsForm.jsx
@@ -9,6 +9,7 @@ import {
 } from '@chakra-ui/react'
 import { Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
+import emailjs from '@emailjs/browser'
 import QuestionsInput from './QuestionsInput'
 
 const QuestionsForm = () => {
@@ -62,11 +63,22 @@ const QuestionsForm = () => {
 
       validationSchema={subscribeSchema}
 
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
+      onSubmit={async (values, { resetForm }) => {
+        await emailjs.send(
+          import.meta.env.VITE_EMAILJS_SERVICE,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_QUESTIONS,
+          values,
+          import.meta.env.VITE_EMAILJS_KEY
+        )
+          .then(response => {
+            console.log(response.status, response.text)
+            resetForm()
+            messageServer(response)
+          })
+          .catch(err => {
+            console.log('Erro: ', err)
+            messageServer(err)
+          })
       }}
 
     >
